eeae2480afc37c62fb615abd9e6b6735be434385
rikki cattermole <pbdadmin@gmail.com>
2014-12-08 20:46:43 +1300

    Adding support for fonts to be modifiable.

--- source/interfaces/devisualization/font/font.d
+++ source/interfaces/devisualization/font/font.d
@@ -30,4 +30,121 @@ interface Font {
     Glyph get(char c);  // gets a glyph for charactor
     Glyph get(dchar c); // "
     Glyph get(wchar c); // "
+
+	final FontWithModifiers modifier() @property {
+		return new FontWithModifiers(this);
+	}
+}
+
+class FontWithModifiers : Font {
+	private {
+		import devisualization.image;
+		Font font_;
+
+		bool italize_;
+		bool bold_;
+		uint width_;
+		ushort kerning_;
+		uint height_;
+		uint lineHeight_;
+		Color_RGBA primary_;
+		Color_RGBA background_;
+	}
+
+	this(Font font) {
+		font_ = font;
+	}
+
+	@property {
+		Font font() {
+			return font_;
+		}
+
+		/**
+		 * Modifies how future getting of glyphs works.
+		 */
+		GlyphModifiers modifiers() {
+			class FontModifiers : GlyphModifiers {
+				void italize() { // makes it italisized
+					italize_ = true;
+				}
+
+				void bold() { // makes it boldenized
+					bold_ = true;
+				}
+
+				void width(uint width) { // scales
+					width_ = width;
+				}
+
+				void kerning(ushort amount) { // adds width but doesn't scale
+					kerning_ = amount;
+				}
+
+				void height(uint amount) { // scales
+					height_ = amount;
+				}
+
+				void lineHeight(uint amount) { // adds height to glyph but doesn't scale
+					lineHeight_ = amount;
+				}
+
+				void color(Color_RGBA primary, Color_RGBA background = null) {
+					primary_ = primary;
+					background_ = background;
+				}
+				
+				void reset() { // reload image for glyph
+					italize_ = false;
+					bold_ = false;
+					width_ = 0;
+					kerning_ = 0;
+					height_ = 0;
+					lineHeight_ = 0;
+					primary_ = null;
+					background_ = null;
+				}
+			}
+
+			return new FontModifiers();
+		}
+	}
+
+	Glyph get(char c) {  // gets a glyph for charactor
+		Glyph ret = font_.get(c);
+		modify(ret);
+		return ret;
+	}
+
+	Glyph get(dchar c) { // "
+		Glyph ret = font_.get(c);
+		modify(ret);
+		return ret;
+	}
+
+	Glyph get(wchar c) { // "
+		Glyph ret = font_.get(c);
+		modify(ret);
+		return ret;
+	}
+
+	private {
+		void modify(Glyph ret) {
+			auto _ = ret.modifiers;
+			if (italize_)
+				_.italize();
+			if (bold_)
+				_.bold();
+			if (width_ > 0)
+				_.width(width_);
+			if (kerning_ > 0)
+				_.kerning(kerning_);
+			if (height_ > 0)
+				_.height(height_);
+			if (lineHeight_ >  0)
+				_.lineHeight(lineHeight_);
+			if (primary_ !is null)
+				_.color(primary_, background_);
+		}
+	}
 }
